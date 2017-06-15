d2432657e8f5bb5a2ffea567c955ab693a105bbe
NEXOS\cavilla <cavilla@nexos-software.com.co>
2017-06-15 14:24:28 -0500

    Updating Source code after implementing passport validation.

--- start/routes/api.js
+++ start/routes/api.js
@@ -3,15 +3,28 @@ var router = express.Router();
 
 /* GET users listing. */
 router.route('/posts')
+    //all posts
+    .get(function(req, res){
+        //temporary solution
+        res.send({message:'TODO: return all posts.'});
+    })
+    .post(function(req, res){
+        //temporary solution
+        res.send({message:'TODO: create a new post.'});
+    });
 
-//all posts
-.get(function(req, res){
-    //temporary solution
-    res.send({message:'TODO: return all posts.'});
-})
-.post(function(req, res){
-    //temporary solution
-    res.send({message:'TODO: create a new post.'});
-});
+router.route('/posts/:id')
+    // returns a particular post
+    .get(function(req,res){
+        res.send({message:'TODO return post with ID ' + req.params.id});
+    })
+    //update existing post.
+    .put(function(req, res){
+        res.send({message:'TODO modify post with ID ' + req.params.id});
+    })
+    //update existing post.
+    .delete(function(req, res){
+        res.send({message:'TODO delete post with ID ' + req.params.id});
+    });
 
 module.exports = router;
