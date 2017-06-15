9561e097bb4fa125640345b149b06bec70857edc
NEXOS\cavilla <cavilla@nexos-software.com.co>
2017-06-15 18:30:37 -0500

    Updating project with code to routes authentication process

--- start/passport-init.js
+++ start/passport-init.js
@@ -13,22 +13,41 @@ module.exports = function(passport){
     });
     
     passport.deserializeUser(function(username,done){
-        return done('we have not implemented this',false);
+        return done(null,users[username]);
     });
 
     passport.use('login', new LocalStrategy({
-        passReqToCallback:true
+            passReqToCallback:true
         },
         function(req, username, password, done){
-            return done('we have not implemented this',false);
+            if(!users[username]){
+                return done('user not found',false);
+            }
+            if(!IsValidPassword(users[username],password)){
+                return done('Invalid password', false);
+            }
+
+            //sucessfully signed in.
+            console.log('sucessfully signed in.');
+            return done(null,users[username]);
         }
     ));
 
     passport.use('signup', new LocalStrategy({
-        passReqToCallback:true //allows us to pass back the entire request to callback
+            passReqToCallback:true //allows us to pass back the entire request to callback
         },
         function(req, username, password, done){
-            return done('we have not implemented this',false);
+            //check if user already exists
+            if(users[username]){
+                return done('username already taken',false);
+            }
+            users[username] ={
+                username: username,
+                password: createHash(password)
+            };
+
+            return done(null,users[username]);
+            
         }
     ));
 
